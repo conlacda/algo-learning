d52dceaff8a52a40290fce440152b1c466eac11e
phamlong15297 <phamlong15297@gmail.com>
2021-05-28 21:04:15 +0700

    them ghi chu isCyclic()

--- graph/practice/graph.py
+++ graph/practice/graph.py
@@ -5,3 +5,6 @@
 # Viết gộp chung tất cả vào 1 rồi khi tới cái nào xóa cái đó đi
 # DFS
 # BFS
+# Kiểm tra đồ thị có cycle không https://www.geeksforgeeks.org/detect-cycle-undirected-graph/
+    * Ý tưởng: duyệt DFS(). Nếu 1 đỉnh có đỉnh kể đã được duyệt nhưng lại không có quan hệ parent thì 2 điểm đó thuộc 1 cycle
+    Bởi vì: 1 đỉnh đó được duyệt từ 2 đỉnh khác nhau. Nếu ko có cycle thì 2 đỉnh đó thuộc 2 nhánh khác nhau
